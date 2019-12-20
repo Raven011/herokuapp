e0e27b0ff2f257326641dbe482bbdda7885c0204
Raven <raven_sharegrave@yahoo.it>
2019-12-20 14:28:35 +0100

    cambio py

--- app.py
+++ app.py
@@ -3,25 +3,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-titolo="Pagina iniziale"
-testo="Ciao mondo!"
-bottone="Più info"
-    return render_template("base.html",
+    titolo="Pagina iniziale"
+    bottone="Info"
+    testo="Hello World"
+    return render_template("base.html", 
         titolo=titolo,
         testo=testo,
         bottone=bottone)
-
 
 @app.route('/info')
 def info():
-titolo="Pagina info"
-testo="Informazioni"
-bottone="Homepage"
-    return render_template("base.html",
+    titolo="Pagina Info"
+    bottone="Homepage"
+    testo="Informazioni"
+    return render_template("base.html", 
         titolo=titolo,
         testo=testo,
         bottone=bottone)
-
 
 if __name__ == '__main__':
     app.run()
